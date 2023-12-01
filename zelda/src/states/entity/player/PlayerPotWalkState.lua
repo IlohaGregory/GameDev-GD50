c1da70aeaccd807f584b6863a63efc49bd24bb1b
IlohaGregory <115503507+IlohaGregory@users.noreply.github.com>
2023-12-01 08:17:05 +0100

    Delete zelda directory

--- zelda/src/states/entity/player/PlayerPotWalkState.lua
+++
@@ -1,8 +0,0 @@
--- Player  walking with pot
-
-PlayerPotWalkState = Class { __includes = EntityWalkState }
-
-function PlayerPotWalkState:init(Player, dungeon)
-    self.entity = Player
-    self.dungeon = dungeon
-end
